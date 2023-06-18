d852b2e34842d38b5d4cf70b1af29bc6ca76c7f2
Sam <prandisamuel99@gmail.com>
2023-06-18 12:40:30 +0200

    modified:   scripts/script.js 	modified:   styles/css/main.css 	modified:   styles/css/main.css.map 	modified:   styles/scss/components/_header.scss

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,24 @@
 const icon = document.querySelector(".menu_icon");
 const menu = document.querySelector(".nav");
+const list = document.querySelector(".list");
+const listItems = document.querySelectorAll(".list li");
 const xmarkUrl = "images/icon-close.svg";
 const hamburgerUrl = "images/icon-hamburger.svg";
 
 icon.addEventListener("click", (e) => {
     if(menu.classList.contains("open")) {
         menu.classList.remove("open");
+        for(const item of listItems) {
+            item.classList.remove("slide");
+        }
         icon.setAttribute("src", hamburgerUrl);
     } else {
         menu.classList.add("open");
+        for(let i = 0; i < listItems.length; i++) {
+            setTimeout(function() {
+                listItems[i].classList.add("slide");
+            }, 150 * i);
+        }
         icon.setAttribute("src", xmarkUrl);
     }
 });
