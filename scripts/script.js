b40e87a7102062ee594ccbd564f8199d043b4969
JustANipple <prandisamuel99@gmail.com>
2023-06-19 11:05:00 +0200

    modified:   index.html 	modified:   scripts/script.js 	modified:   styles/css/main.css 	modified:   styles/css/main.css.map 	modified:   styles/scss/components/_creations.scss

--- scripts/script.js
+++ scripts/script.js
@@ -22,3 +22,54 @@ icon.addEventListener("click", (e) => {
         icon.setAttribute("src", xmarkUrl);
     }
 });
+
+const creationsCont = document.querySelector(".creations_wrapper");
+const mobImages = [
+    "images/mobile/image-deep-earth.jpg",
+    "images/mobile/image-night-arcade.jpg",
+    "images/mobile/image-soccer-team.jpg",
+    "images/mobile/image-grid.jpg",
+    "images/mobile/image-from-above.jpg",
+    "images/mobile/image-pocket-borealis.jpg",
+    "images/mobile/image-curiosity.jpg",
+    "images/mobile/image-fisheye.jpg"
+];
+const desImages = [
+    "images/desktop/image-deep-earth.jpg",
+    "images/desktop/image-night-arcade.jpg",
+    "images/desktop/image-soccer-team.jpg",
+    "images/desktop/image-grid.jpg",
+    "images/desktop/image-from-above.jpg",
+    "images/desktop/image-pocket-borealis.jpg",
+    "images/desktop/image-curiosity.jpg",
+    "images/desktop/image-fisheye.jpg"
+];
+const titles = [
+    "deep earth",
+    "night arcade",
+    "soccer team vr",
+    "the grid",
+    "from up above vr",
+    "pocket borealis",
+    "the curiosity",
+    "make it fisheye"
+];
+
+for(let i = 0; i < titles.length; i++) {
+    const wrapper = document.createElement("div");
+    wrapper.setAttribute("class", "creation_block");
+    wrapper.innerHTML = 
+    `
+    <picture>
+    <source
+    srcset=${desImages[i]}
+    media="(min-width: 768px)">
+    <img 
+    src=${mobImages[i]}
+    alt=""
+    aria-hidden="true">
+    </picture>
+    <h3 class="title">${titles[i]}</h3>
+    `;
+    creationsCont.appendChild(wrapper);
+}
